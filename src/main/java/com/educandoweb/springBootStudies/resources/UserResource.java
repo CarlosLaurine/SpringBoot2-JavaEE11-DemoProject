b31e13b1cd0588f2cf74064039cabcf51b1a1bca
Carlos Laurine <carlos.pinho@ucsal.edu.com.br>
2021-03-20 02:54:54 -0300

    User Entity + User Resource Implemented + UserResource ResponseTest at Tomcat port 8090 /users + Implementation Comments

---
+++ src/main/java/com/educandoweb/springBootStudies/resources/UserResource.java
@@ -0,0 +1,34 @@
+package com.educandoweb.springBootStudies.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.springBootStudies.entities.User;
+
+//Putting an annotation to signal that this class is a web resource implemented by a Rest Controller
+@RestController
+
+//Setting a name to the Resource through Resource Path
+@RequestMapping (value = "/users")
+
+public class UserResource {
+	
+	//End Point Method to access users
+	//ResponseEntity<> = Spring Specific Return Type to return Responses from Web Requests
+	
+	//Setting @GetMapping Annotation to indicate that the following method will respond to HTTP protocol "get" requisition
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		
+		User user = new User(1L, "john", "john@gmail.com", "99999-7777", "12345");
+		
+		/*Calling ResponseENTITY.ok() to successfully return the response at HTTP protocol 
+		  and .body() to also return the response body */
+		
+		return ResponseEntity.ok().body(user);
+		
+	}
+
+}
